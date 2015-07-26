2ab854c44741c3ae30177378c9a0dad1af7a67f8
Nsnap01 <nsnap01@Matthwes-MacBook-Air.local>
2015-07-26 15:27:03 -0400

    Programming#2
    My own code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  InverseX <- NULL #empty variable for the inversion.
+  set <- function(y){
+    x <<- y
+    InverseX <<- NULL
+  }
+  get <- function() x #pulls the input matrix.
+  setTHEx <- function(solve) InverseX <<- solve #sets the inverse matrix.
+  getTHEx <- function() InverseX #pulls the inverse matrix.
+  list(set = set, get = get,
+       setTHEx = setTHEx,
+       getTHEx = getTHEx)
 }
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  InverseX <- x$getTHEx() #pulls value from cache if not empty; skips SolveInverse.
+  if(!is.null(InverseX)){
+    message("getting cached data")
+    return(InverseX) #displays the value in the cache.
+  }
+  SolveInverse <- x$get()
+  InverseX <- solve(SolveInverse, ...) #calculates the inverse if the cache was empty.
+  x$setTHEx(InverseX) #sets the value in the cache.
+  return(InverseX) #displays the value calculated.
 }
